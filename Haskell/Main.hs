d7e2b30ffefa102f94711fd34bf8ff0ec8439acb
wineone <matheusmat102@gmail.com>
2019-05-10 16:35:30 -0300

    olha ai monitor

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -1,33 +1,74 @@
 import Estruturas
+import System.IO.Unsafe
+import Print
+
+fases :: IO Personagem ->  IO Personagem
+fases per =   do
+              let p = unsafePerformIO per
+              return p            
+
+menu :: IO Personagem -> IO Personagem
+menu p =  do
+        printa
+        op <- readLn::IO Int
+        if(op == 1) then
+            menu (fases p)
+        else 
+            menu p
 
 
-escolheDes :: Personagem -> Int -> Personagem
-escolheDes (Personagem {nome = a, idade = b}) c | (c == 1) = (Personagem a (b+10)) -- funções do jogo aqui
-                                            | (c == 2) = (Personagem a (b + 25)) -- funções do jogo aqui
-                                            | otherwise = (Personagem a b) -- funções do jogo aqui
-
-loop :: Personagem -> (IO ()) -> IO ()
-loop p  func =  do
-                func
-                print p
-                a <- readLn::IO Int
-                loop (escolheDes p a) func
-                return ()
-
-
-a ::  IO ()
-a = do
-    putStrLn "primeira função de testes"
-    return ()
-
-b ::  IO ()
-b = do
-    putStrLn "segunda função de testes"
-    return ()
-
-
-main:: IO ()
+main :: IO ()
 main =  do
-        let p = Personagem "matheus" 0
-        loop p b
+        nome <- getLine
+        let p = Personagem nome 21
+        menu (p :: IO Personagem)
         return ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- escolheDes :: Personagem -> Int -> Personagem
+-- escolheDes (Personagem {nome = a, idade = b}) c | (c == 1) = (Personagem a (b+10)) -- funções do jogo aqui
+--                                                 | (c == 2) = (Personagem a (b + 25)) -- funções do jogo aqui
+--                                                 | otherwise = (Personagem a b) -- funções do jogo aqui
+
+-- loop :: Personagem -> (IO ()) -> IO ()
+-- loop p  func =  do
+--                 func
+--                 print p
+--                 a <- readLn::IO Int
+--                 loop (escolheDes p a) func
+--                 return ()
+
+-- a ::  IO ()
+-- a = do
+--     putStrLn "primeira função de testes"
+--     return ()
+
+-- b ::  IO ()
+-- b = do
+--     putStrLn "segunda função de testes"
+--     return ()
+
+
+-- main:: IO ()
+-- main =  do
+--         let p = Personagem "matheus" 0
+--         loop p b
+--         return ()
---
+++ Haskell/Print.hs
@@ -0,0 +1,9 @@
+module Print where
+
+printa :: IO()
+printa =    do
+            putStrLn "opcao 1"
+            putStrLn "opcao 2"
+            putStrLn "opcao 3"
+            putStrLn "opcao 4"
+            putStrLn "opcao 5"
