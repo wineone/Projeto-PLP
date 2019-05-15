d83cca5eafbcdd0c230fad5764123001b215e5e3
matheus lisboa <matheus.lisboa.santos@ccc.ufcg.edu.br>
2019-05-15 16:06:10 -0300

    AVISO IMPORTANTE NO GRUPO DO TRABALHO

--- Haskell/Fases.hs
+++ Haskell/Fases.hs
@@ -1,24 +1,28 @@
 module Fases where
 
 import Estruturas
+import System.IO.Unsafe
+import Print
+
 
 fases :: Personagem -> IO Personagem
 fases per = do
             let a = unsafePerformIO (printaFases per) 
             if(a == 1) then do
-                lobby per --função que chama o manguezal
+                return per --função que chama o manguezal
             else if (a == 2) then do
-                lobby per -- função que chama a casa
+                return per -- função que chama a casa
             else if (a == 3) then do
-                lobby per -- função que chama o jogoses Voraz
+                return per -- função que chama o jogoses Voraz
             else if (a == 4) then do
-                lobby per -- função que cham o piloto
+                return per -- função que cham o piloto
             else if (a == 5) then do
-                lobby per -- função que chama a Area 51
+                return per -- função que chama a Area 51
             else if (a == 6) then do 
-                lobby per -- função qeu chama o BOSS
+                return per -- função qeu chama o BOSS
             else if (a == 7) then do
-                lobby per -- função que sai
-            else
+                return per -- função que sai
+            else do
+                putStrLn "Opção invalida"
                 fases per
             
--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -7,15 +7,15 @@ lobby :: Personagem -> IO Personagem
 lobby per = do
             let o = unsafePerformIO (printLobby per)
             if(o == 1) then do
-                fases per -- função que chama a fase
+                lobby (unsafePerformIO (fases per)) -- função que chama a fase
             else if (o == 2) then do
-                fases per -- função que chama a loja
+                lobby (unsafePerformIO (fases per)) -- função que chama a loja
             else if (o == 3) then do
-                fases per -- função que chama a bolsa
+                lobby (unsafePerformIO (fases per)) -- função que chama a bolsa
             else if (o ==  4) then do
-                fases per -- função que chama a bolsa completa
+                lobby (unsafePerformIO (fases per)) -- função que chama a bolsa completa
             else if (o == 5) then do
-                fases per -- função que chama a creditos
+                lobby (unsafePerformIO (fases per)) -- função que chama a creditos
             else if (o == 6) then do
                 putStrLn "quereu sair"
                 return per -- função que chama a saida
