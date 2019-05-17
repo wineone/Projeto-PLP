8c8c9626172d46873bd56d82feb8b8cdb71eb57a
wineone <matheusmat102@gmail.com>
2019-05-17 17:09:02 -0300

    entidades adicionadas

--- Haskell/Entidades.hs
+++ Haskell/Entidades.hs
@@ -34,4 +34,178 @@ manguezal = Fase "Manguezal" "Se não tiver usando botas, vai se sujar de lama" 
 -- ++++++++++++++++  FIM DA FASE 1 ++++++++++++++++++++++++++++
 
 
+-- ++++++++++++++++++++++++++++++++++++ FASE 2 +++++++++++++++++++++++++++++++++++++
 
+
+meia :: Inimigo
+meia = Inimigo "Meia Chulelenta" "Bem fedida mesmo" 20 20 5 5 2 2
+
+bixo :: Inimigo
+bixo = Inimigo "Bixo Papão" "De baixo da sua cama, é... sempre esteve" 55 55 12 10 8 2
+
+parede :: Inimigo
+parede = Inimigo "Parede Mofada" "Lança esporos que podem te deixar doidão rs" 40 40 8 5 5 3
+ 
+ventila :: Inimigo 
+ventila = Inimigo "Ventilador de Teto" "Ele vai cair em cima de você, eu sei que você já imaginou isso" 60 60 15 10 5 5
+
+chao :: Inimigo
+chao = Inimigo "Chão Movediço" "Quando você entrar o chão quase lhe engoliu" 35 35 7 7 7 5
+
+louca :: Inimigo
+louca = Inimigo "Louça Suja" "Ela passou tanto tempo na pia que criou vida" 50 50 10 10 10 7
+
+gru1Fase2 :: GrupoDeInimigos
+gru1Fase2 = GrupoDeInimigos 3 200 [meia,meia,meia]
+
+gru2Fase2 :: GrupoDeInimigos
+gru2Fase2 = GrupoDeInimigos 3 300 [meia,bixo,parede]
+
+gru3Fase2 :: GrupoDeInimigos
+gru3Fase2 = GrupoDeInimigos 3 400 [bixo,ventila,chao]
+
+gru4Fase2 :: GrupoDeInimigos
+gru4Fase2 = GrupoDeInimigos 4 400 [louca,chao,meia,meia]
+
+gru5Fase2 :: GrupoDeInimigos
+gru5Fase2 = GrupoDeInimigos 4 500 [meia,meia,louca,ventila]
+
+casa :: Fase
+casa = Fase "Casa da Mãe Joana" "Tem coisas aqui que os deixariam enojados" 5 [gru1Fase2,gru2Fase2,gru3Fase2,gru3Fase2,gru4Fase2,gru5Fase2]
+
+
+-- +++++++++++++++++++++++++++ FIM DA FASE 2 +++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+-- +++++++++++++++++++++++++++++++++++++++++++ FASE 3 +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+seneca :: Inimigo
+seneca = Inimigo "Seneca" "Não sou o Soneca, sou o Seneca" 70 70 8 8 7 8
+
+reimite :: Inimigo
+reimite = Inimigo "reimite" "Onde está a pinga" 100 100 10 15 8 14
+
+pita :: Inimigo 
+pita = Inimigo "Pita" "Perdeu as perna mas passa bem" 85 85 10 15 7 8
+
+katenisse :: Inimigo
+katenisse = Inimigo "Katenisse" "Ela é o tordo, visse" 90 90 12 11 12 13
+
+gueiou :: Inimigo
+gueiou = Inimigo "Gueiou" "Eu estou apaixonado por Katenisse" 80 80 9 12 7 10
+
+esnou :: Inimigo
+esnou = Inimigo "Esnou" "Sou o presidente, talkei" 80 80 16 8 8 15
+
+finique :: Inimigo 
+finique = Inimigo "Finique" "Perdeu a cabeça, algo compreesível" 95 95 13 15 12 11
+
+
+gru1Fase3 :: GrupoDeInimigos
+gru1Fase3 = GrupoDeInimigos 2 600 [seneca,reimite]
+
+gru2Fase3 :: GrupoDeInimigos
+gru2Fase3 = GrupoDeInimigos 2 600 [pita,katenisse]
+
+gru3Fase3 :: GrupoDeInimigos
+gru3Fase3 = GrupoDeInimigos 3 600 [pita,katenisse, gueiou]
+
+gru4Fase3 :: GrupoDeInimigos
+gru4Fase3 = GrupoDeInimigos 3 600 [esnou,katenisse,finique]
+
+
+jogosVoraz :: Fase
+jogosVoraz = Fase "Jogoses Voraz" "Tributos contra os zinimigos" 4 [gru1Fase3,gru2Fase3,gru3Fase3,gru4Fase3]
+
+-- ++++++++++++++++++++++++ FIM DA FASE 3 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- ++++++++++++++++++++++++ COMEÇO DA FASE 4 +++++++++++++++++++++++++++++++++++++++++++++++++
+
+papaco :: Inimigo
+papaco = Inimigo "Papaco" "Um pistoleiro chamado Papaco" 100 100 15 15 15 15
+
+pauloGuedes :: Inimigo 
+pauloGuedes = Inimigo "Paulo Guedes" "Eu vo privatiza ese jogo" 110 110 20 20 20 20
+
+nemesis :: Inimigo
+nemesis = Inimigo "Nemesis" "Fei que doi" 150 150 25 25 25 25
+
+conexaoRuim :: Inimigo
+conexaoRuim = Inimigo "Conexão Ruim" "Seu maior inimigo em dias de jogar ou de enviar o trabalho de última hora" 150 150 20 15 10 15 
+
+oCaraAlho :: Inimigo
+oCaraAlho = Inimigo "O Cara Alho" "Só um cara com mal hálito" 125 125 25 10 15 25
+
+pinky :: Inimigo
+pinky = Inimigo "Pinky" "Irmão Do Cérebro" 150 150 20 25 10 20
+
+cerebro :: Inimigo
+cerebro = Inimigo "Cérebro" "Irmão do Pinky" 150 150 20 20 25 10
+
+gru1Fase4 :: GrupoDeInimigos
+gru1Fase4 = GrupoDeInimigos 3 700 [papaco,papaco,pauloGuedes]
+
+gru2Fase4 :: GrupoDeInimigos
+gru2Fase4 = GrupoDeInimigos 2 800 [nemesis,pauloGuedes]
+
+gru3Fase4 :: GrupoDeInimigos
+gru3Fase4  = GrupoDeInimigos 3 900 [nemesis,nemesis]
+
+gru4Fase4 :: GrupoDeInimigos
+gru4Fase4 = GrupoDeInimigos 3 800 [pinky,cerebro,conexaoRuim]
+
+gru5Fase4 :: GrupoDeInimigos
+gru5Fase4 = GrupoDeInimigos 3 900 [conexaoRuim,oCaraAlho,cerebro]
+
+area51 :: Fase
+area51 = Fase "Piloto" "Fase de testes, cuidado senão ela ele te derruba" 5 [gru1Fase4,gru2Fase4,gru3Fase4,gru4Fase4,gru5Fase4]
+
+-- +++++++++++++++++++++++++++++++++++++++++ FIM DA FASE 4 +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- +++++++++++++++++++++++++++++++++++ INICIO DA FASE 5 +++++++++++++++++++++++++++++++
+
+gravida :: Inimigo 
+gravida = Inimigo "Grávida de Taubaté" "Grávida (quase)." 250 250 30 30 25 30
+
+filho :: Inimigo
+filho = Inimigo "Filho da Grávida" "Filho de mentirinha" 100 100 15 15 10 15
+
+etBilu :: Inimigo
+etBilu = Inimigo "Et Bilu" "Busquem conhecimento" 150 150 25 25 35 20
+
+jovem :: Inimigo
+jovem = Inimigo "Jovem" "Ninguém gosta de Jovem" 270 270 30 30 35 25
+
+cowboy :: Inimigo 
+cowboy = Inimigo "Cowboy do Espaço" "See you Space Cowboy" 100 100 15 15 15 15
+
+gru1Fase5 :: GrupoDeInimigos
+gru1Fase5 = GrupoDeInimigos 1000 2 [gravida,filho]
+
+gru2Fase5 :: GrupoDeInimigos
+gru2Fase5 = GrupoDeInimigos 1100 2 [etBilu,jovem]
+
+gru3Fase5 :: GrupoDeInimigos
+gru3Fase5 = GrupoDeInimigos 1000 1 [cowboy]
+
+gru4Fase5 :: GrupoDeInimigos
+gru4Fase5 = GrupoDeInimigos 1200 3 [gravida,jovem,etBilu]
+
+area :: Fase
+area = Fase "Área 51" "Ninguém sabe o que tem aí dentro" 4 [gru1Fase5,gru2Fase5,gru3Fase5,gru4Fase5]
+
+-- ++++++++++++++++++++++++++++++++++++ FIM DA FASE 5 +++++++++++++++++++++++++++++++++++++++++++
+
+-- +++++++++++++++++++++++++++++ COMEÇO DO BOSS ++++++++++++++++++++++++++++++++++++++
+
+
+boss :: Inimigo
+boss = Inimigo "Light Theme IDE" "Fiquei ceguin" 500 500 50 50 50 50
+
+gBoss :: GrupoDeInimigos
+gBoss = GrupoDeInimigos 1 50000000 [boss]
+
+faseBoss :: Fase
+faseBoss = Fase "Última fase do jogo" "Se ligue não" 1 [gBoss]
+
+-- +++++++++++++++++++++++++++++++++ FIM DO BOSS ++++++++++++++++++++++++++++++++++++++++++++
--- Haskell/Fases.hs
+++ Haskell/Fases.hs
@@ -12,19 +12,19 @@ fases :: Personagem -> IO Personagem
 fases per = do
             let a = unsafePerformIO (printaFases per) 
             if(a == 1) then do
-                return (unsafePerformIO (mapa per manguezal))
+                return (unsafePerformIO (mapa per manguezal)) -- função que chama o manguezal
             else if (a == 2) then do
-                return per -- função que chama a casa
+                return (unsafePerformIO (mapa per manguezal)) -- função que chama a casa
             else if (a == 3) then do
-                return per -- função que chama o jogoses Voraz
+                return (unsafePerformIO (mapa per manguezal)) -- função que chama o jogoses Voraz
             else if (a == 4) then do
-                return per -- função que cham o piloto
+                return (unsafePerformIO (mapa per manguezal)) -- função que cham o piloto
             else if (a == 5) then do
-                return per -- função que chama a Area 51
+                return (unsafePerformIO (mapa per manguezal)) -- função que chama a Area 51
             else if (a == 6) then do 
-                return per -- função qeu chama o BOSS
+                return (unsafePerformIO (mapa per manguezal)) -- função qeu chama o BOSS
             else if (a == 7) then do
-                return per -- função que sai
+                return (unsafePerformIO (mapa per manguezal)) -- função que sai
             else do
                 putStrLn "Opção invalida"
                 fases per
