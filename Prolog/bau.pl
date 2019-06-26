ac67171c70a701afd0952aed38ec13fda62a5367
Igor Pereira <igor.pereira@ccc.ufcg.edu.br>
2019-06-26 18:09:36 -0300

    Novas Modificacoes em Bolsa

--- Prolog/bau.pl
+++ Prolog/bau.pl
@@ -9,20 +9,53 @@ addPocao(Bolsa,Pocao) :- Bolsa = [Pocao|X, _, _].
 addArmadura(Bolsa,Armadura) :- Bolsa = [_, Armadura|Y, _].
 addArma(Bolsa,Arma) :- Bolsa = [_, _, Arma|Z].
 
-
+/* Funcoes que retornam apenas um array da bolsa */
 pegaPocao(Per,Pocao) :- Per =[_, _, _,_, _, _, _, _, _, _, [Pocao,_,_]].
 pegaArmadura(Per,Armadura) :- Per =[_, _, _,_, _, _, _, _, _, _, [_,Armadura,_]].
 pegaArma(Per,Arma) :- Per = [_, _, _,_, _, _, _, _, _, _, [_,_,Arma]].
 
+/* funcao para pegar tamanho de algum array length(Pocao, Retorno)*/
+
+/* REMOVE ELEMENTO DE UM ARRAY*/
+removePocao(Element, [Element|Resto], Resto).
+removePocao(Element, [H|Resto], [H|Saida]):- removePocao(Element,Resto,Saida).
+
+/* ALTERA O ARRAY DE POCAO DO PERSONAGEM */
+renovaPocaoPersonagem(Per,Pocao,NewPer) :- NewPer = Per ,NewPer = [_, _, _,_, _, _, _, _, _, _, [Pocao,_,_]].
 
 /* [(Nome, Descricao, _, Dano, Forca, Agilidade) |Teste] */
 percorrePocao(Ind, []) :- write("").
 percorrePocao(Ind, Pocao) :- print:pocoes(Ind, Pocao).
 
-bolsaOp(1,Per,NewPer) :- write("Vizualizar Uma Pocao"),pegaPocao(Per,Pocao),percorrePocao(1,Pocao),nl,gerenciaBolsa(Per,NewPer).
-bolsaOp(2,Per,NewPer) :- write("Usar uma Pocao"),gerenciaBolsa(Per,NewPer).
-bolsaOp(3,Per,NewPer) :- write("Jogar uma pocao fora"),gerenciaBolsa(Per,NewPer).
+bolsaOp(1,Per,NewPer) :- pegaPocao(Per,Pocao),
+                        percorrePocao(1,Pocao),
+                        nl,
+                        digite,
+                        gerenciaBolsa(Per,NewPer).
+
+
+bolsaOp(2,Per,NewPer) :- pegaPocao(Per,Pocao),
+                        percorrePocao(1,Pocao),
+
+                        write("Usar uma Pocao"),gerenciaBolsa(Per,NewPer).
+
+bolsaOp(3,Per,NewPer) :- pegaPocao(Per,Pocao), %funcao que retorna o array pocao do personagem
+                        percorrePocao(1,Pocao),%Funcao que printa as pocoes
+                        lerNumero(Indice),
+                        getElement(Pocao, Indice, Element),%funcao que pega o elemento na posicao do indice
+                        removePocao(Element,Pocao,Saida), %funcao que remove a pocao do array principal
+                        write("Essa pocao e a antiga\n\n"),
+                        write(Saida),%ate aqui funcionas
+                        write("\n1\n\n"),
+                        /*renovaPocaoPersonagem(Per,Saida,Testa),
+                        pegaPocao(Testa,NewPocao),
+                        write("\n\nEssa pocao e do novo per\n\n"),
+                        write(NewPocao),*/
+                        digite,
+                        gerenciaBolsa(Per,Testa).
+                        
 bolsaOp(4,Per,NewPer) :- NewPer = Per.
+
 bolsaOp(_,Per,NewPer) :- write("NUMSEI\n\n"),gerenciaBolsa(Per,NewPer).
 /* [_, _, _,_, _, _, _, _, _, _, [Pocao,Armadura,Arma]] */
 
