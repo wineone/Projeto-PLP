49bbc1c74de18c5b34655a8da597071930adff81
Bruno Henrique <bhenriquecg@gmail.com>
2019-06-27 14:21:25 -0300

    add itens para o bau

--- Prolog/loja.pl
+++ Prolog/loja.pl
@@ -6,41 +6,43 @@
 :- use_module(bau).
 
 
-lojaOp(1,[A,B,C,D,E,F,G,H,I,J,K,[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo) :-
+lojaOp(1,[A,B,C,D,E,F,G,H,I,J,[_,_,PerArmas]],[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo) :-
      print:printDinheiro(H),%mostra o dinheiro atual do personagem   
      print:armas(1,ListAr),%funcao que lista as armas
      print:leOpArma(Opcao),%funcao que le a opção escolhida pelo usuario
      util:getElement(ListAr,Opcao,[Na,Da,Pa,Daa,Fa,Aa]),% pega a opcao escolhida pelo usuario
      ((H >= Pa) -> % Se o personagem tiver dinheiro suficiente, a compra é feita
      Ndinheiro is (H - Pa),%Diminui do dinheiro do persogem o valor do item
-     bau:addArma([Na,Da,Pa,Daa,Fa,Aa],K,FF),
-     Aux = [A,B,C,D,E,F,G,Ndinheiro,I,J,K],
+     bau:insereFim([Na,Da,Pa,Daa,Fa,Aa],PerArmas,SaidaArmas),
+     Aux = [A,B,C,D,E,F,G,Ndinheiro,I,J,[_,_,SaidaArmas]],
      print:printComprou(Na);%printa o item comprado
-     writeln("\nDinheiro insuficiente\n"),Aux = [A,B,C,D,E,F,G,H,I,J,K],util:digite),
+     writeln("\nDinheiro insuficiente\n"),Aux = [A,B,C,D,E,F,G,H,I,J,[_,_,PerArmas]],util:digite),
      loja(Aux,[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo).
 
-lojaOp(2,[A,B,C,D,E,F,G,H,I,J,K],[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo) :- 
+lojaOp(2,[A,B,C,D,E,F,G,H,I,J,[_,PerArmaduras,_]],[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo) :- 
     print:printDinheiro(H), 
     print:armaduras(1,ListaArmadura),
     print:leOpArmadura(Opcao),
     util:getElement(ListaArmadura,Opcao,[Nar,Dar,Par,Daar,Far,Aar,Var]),
     ((H >= Par) ->
     Ndinheiro is (H - Par),
-    Aux = [A,B,C,D,E,F,G,Ndinheiro,I,J,K],
+    bau:insereFim([Nar,Dar,Par,Daar,Far,Aar,Var],PerArmaduras,SaidaArmaduras),
+    Aux = [A,B,C,D,E,F,G,Ndinheiro,I,J,[_,SaidaArmaduras,_]],
     print:printComprou(Nar);
-    writeln("\nDinheiro insuficiente\n"),Aux = [A,B,C,D,E,F,G,H,I,J,K],util:digite),
+    writeln("\nDinheiro insuficiente\n"),Aux = [A,B,C,D,E,F,G,H,I,J,[_,PerArmaduras,_]],util:digite),
     loja(Aux,[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo).
 
-lojaOp(3,[A,B,C,D,E,F,G,H,I,J,K],[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo) :- 
+lojaOp(3,[A,B,C,D,E,F,G,H,I,J,[PerPocoes,_,_]],[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo) :- 
     print:printDinheiro(H), 
     print:pocoes(1,ListaPocoes),
     print:leOpPocao(Opcao),
     util:getElement(ListaPocoes,Opcao,[Np,Dp,Pp,Vp]),
     ((H >= Pp) -> 
     Ndinheiro is (H - Pp),
-    Aux = [A,B,C,D,E,F,G,Ndinheiro,I,J,K],
+    bau:insereFim([Np,Dp,Pp,Vp],PerPocoes,Saida),
+    Aux = [A,B,C,D,E,F,G,Ndinheiro,I,J,[Saida,_,_]],
     print:printComprou(Np);
-    writeln("\nDinheiro insuficiente\n"),Aux = [A,B,C,D,E,F,G,H,I,J,K],util:digite),
+    writeln("\nDinheiro insuficiente\n"),Aux = [A,B,C,D,E,F,G,H,I,J,[PerPocoes,_,_]],util:digite),
     loja(Aux,[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo).
 
 lojaOp(4,Per,[QtdArmas , ListAr , QtdArmadura, ListaArmadura,QtdPocoes ,ListaPocoes],Novo) :- Novo = Per.
